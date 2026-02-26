eddb1b6589e5dbb282b92dc69ea909cb11625056
Saul Dominguez <dguezs@gmail.com>
2026-02-26 10:57:07 -0600

    chore:cleaning code

--- backend/server.js
+++ backend/server.js
@@ -3,63 +3,31 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-/**
- * Definición de las opciones de Cors
- * permite conexiones desde cualquier origen
- * métodos de HTTP 
- * Expone el encabezado Etag para ser leido desde el cliente
- */
 const corsOptions = {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     exposedHeaders: ['Etag']
   };
 
-// Middleware para servir archivos estáticos
-app.use(express.static('public'));
 
-/**
- * Cros-origin Reference Sharing 
- * Mecanismo que permite o restringe las solicitudes HTTP 
- * a recursos de diferentes orígenes (dominios, protocolos o puertos) 
- * distintos al origen desde el cual se carga la página web.
- * 
- * Política de seguridad que ayuda a evitar ataques como el 
- * Cross-Site Request Forgery
- */
 app.use(cors(corsOptions));
 
-/**
- * Definición de un endpoint de nuestra API
- */
+
 app.get('/data', (req, res) => {
     const data = {
-        message: "Hola, este es la información en tu Caché :|"
+        message: "Hola, este es la información en tu Caché :("
     };
 
-    //ETag basado en el contenido
     const etag = `"${Buffer.from(JSON.stringify(data)).toString('base64')}"`;
     console.log(`etag ${etag}`);
-
-    // Verificar si el cliente tiene el ETag
+    
     if (req.headers['if-none-match'] === etag) {
-        // El recurso no ha cambiado, responde con 304 Not Modified
         res.status(304).end();
     } else {
-        // El recurso ha cambiado, responde con el ETag
+        
         res.setHeader('ETag', etag);
         res.json(data);
     }
-});
-
-app.get('/pokemon', (req, res) => {
-    const pokemon = {
-        nombre: "pikachu",
-        tipo: "electrico",
-        poder: "rayos"
-    };
-
-    res.json(pokemon);
 });
 
     app.listen(port, () => {
